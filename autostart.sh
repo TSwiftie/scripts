2d1a2712efaaba938cdfcd5c89153624953b2f56
TSwiftie <tswiftie@foxmail.com>
2020-09-12 15:11:04 +0000

    update

--- autostart.sh
+++ autostart.sh
@@ -11,6 +11,7 @@ picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -
 xautolock -time 10 -locker '/usr/bin/lock' -corners ---- -cornersize 30 &
 nm-applet &
 blueman-applet &
+optimus-manager-qt &
 caffeine-indicator &
 flameshot &
 #xfce4-power-manager &
