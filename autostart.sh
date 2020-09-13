48f8b51a46c2030a980d18ccf55b84cff64ff03a
TSwiftie <tswiftie@foxmail.com>
2020-09-13 19:41:57 +0000

    update

--- autostart.sh
+++ autostart.sh
@@ -2,11 +2,11 @@
 
 /bin/bash ~/scripts/dwm-status.sh &
 #feh --bg-fill ~/Pictures/wallhaven1.jpg &
+wmname LG3D
 ~/scripts/dualmon.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
-#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-wmname LG3D
-picom -b
+picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+#picom -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
 xautolock -time 10 -locker '/usr/bin/lock' -corners ---- -cornersize 30 &
