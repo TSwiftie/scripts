cbd6196342663ef669bc1a458beef609313a50e6
TSwiftie <tswiftie@foxmail.com>
2020-09-13 12:55:53 +0000

    update

--- autostart.sh
+++ autostart.sh
@@ -4,8 +4,8 @@
 #feh --bg-fill ~/Pictures/wallhaven1.jpg &
 ~/scripts/dualmon.sh &
 /bin/bash ~/scripts/wp-autochange.sh &
-picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
-#picom -b
+#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
+picom -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
 xautolock -time 10 -locker '/usr/bin/lock' -corners ---- -cornersize 30 &
@@ -14,6 +14,7 @@ blueman-applet &
 caffeine-indicator &
 flameshot &
 xfce4-power-manager &
+optimus-manager-qt &
 #xfce4-volumed-pulse &
 #/bin/bash ~/scripts/run-mailsync.sh &
 ~/scripts/autostart_wait.sh &
