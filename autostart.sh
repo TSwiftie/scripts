b7cec373f8a25bc43786fe525917995dd47c202d
TSwiftie <tswiftie@foxmail.com>
2020-11-16 20:37:07 +0000

    update

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 /bin/bash ~/scripts/dwm-status.sh &
-feh --bg-fill ~/Pictures/wallpapers/wallhaven/wallhaven-39dxg3.png &
+feh --bg-fill ~/Pictures/wallpapers/wallhaven/wallhaven-9mxz8k.jpg &
 wmname LG3D
 ~/scripts/dualmon.sh &
 #/bin/bash ~/scripts/wp-autochange.sh &
@@ -15,7 +15,7 @@ blueman-applet &
 caffeine-indicator &
 flameshot &
 xfce4-power-manager &
-optimus-manager-qt &
+#optimus-manager-qt &
 #xfce4-volumed-pulse &
 #/bin/bash ~/scripts/run-mailsync.sh &
 ~/scripts/autostart_wait.sh &
