61756c6a3baf9502fb8d5a56937785c6d8eddd3c
TSwiftie <tswiftie@foxmail.com>
2020-11-26 22:45:42 +0000

    update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -5,7 +5,6 @@ feh --bg-fill ~/Pictures/wallpapers/wallhaven/wallhaven-9mxz8k.jpg &
 wmname LG3D
 ~/scripts/dualmon.sh &
 #/bin/bash ~/scripts/wp-autochange.sh &
-#picom -o 0.95 -i 0.88 --detect-rounded-corners --vsync --blur-background-fixed -f -D 5 -c -b
 picom -b
 /bin/bash ~/scripts/tap-to-click.sh &
 /bin/bash ~/scripts/inverse-scroll.sh &
@@ -16,6 +15,4 @@ caffeine-indicator &
 flameshot &
 xfce4-power-manager &
 optimus-manager-qt &
-#xfce4-volumed-pulse &
-#/bin/bash ~/scripts/run-mailsync.sh &
 ~/scripts/autostart_wait.sh &
