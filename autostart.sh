6aa109c7c31b57b0c130e2f9d3508ded8f650801
TSwiftie <tswiftie@foxmail.com>
2020-12-06 13:45:27 +0000

    update

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,7 @@
 #dwm-status
 /bin/bash ~/scripts/dwm-status.sh &
 #壁纸
-feh --bg-fill ~/Pictures/wallpapers/wallhaven/wallhaven-9mxz8k.jpg &
+feh --bg-fill ~/Pictures/wallpapers/wallhaven/wallhaven-v9x6gm.jpg &
 #修改wmname
 wmname LG3D
 #显示器
