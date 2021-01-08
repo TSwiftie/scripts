d11ec2d883486d0496031e0e8d42503aae37ad9d
TSwiftie <tswiftie@foxmail.com>
2021-01-08 17:45:32 +0000

    change the default wallpaper

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,7 @@
 #dwm-status
 /bin/bash ~/scripts/dwm-status.sh &
 #壁纸
-feh --bg-fill ~/Pictures/wallpapers/wallhaven/wallhaven-8395mk.png &
+feh --bg-fill ~/Pictures/wallpapers/wallhaven/wallhaven-6oq5lq.jpg &
 #修改wmname
 wmname LG3D
 #显示器
