bf2bce778ddd2f78fb2133934736661844a20b15
TSwiftie <tswiftie@foxmail.com>
2021-03-06 18:58:18 +0000

    update

--- dualmon.sh
+++ dualmon.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #xrandr --auto --output HDMI-1 --same-as eDP-1 --size 1920x1080
-xrandr --output eDP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP-1-1 --off --output HDMI-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
 #xrandr --auto --output HDMI-1  --right-of DP-2 --size 1920x1080
