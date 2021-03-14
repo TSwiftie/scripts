6008a765940794584c752920f2591c9ea82198a9
TSwiftie <tswiftie@foxmail.com>
2021-03-14 16:36:16 +0000

    change the default wallpaper

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,7 @@
 #dwm-status
 /bin/bash ~/scripts/dwm-status.sh &
 #壁纸
-feh --bg-fill ~/Pictures/wallpapers/wallhaven/wallhaven-pk3kde.jpg &
+feh --bg-fill ~/Pictures/wallpapers/wallhaven/wallhaven-8oojm1.jpg &
 #修改wmname
 wmname LG3D
 clash &
